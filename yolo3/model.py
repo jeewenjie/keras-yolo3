ba1c525c5b3dfad3d576728a919cb47c54c6121e
Jee Wen Jie <40534826+jeewenjie@users.noreply.github.com>
2019-10-11 17:54:17 +0800

    Update model.py

--- yolo3/model.py
+++ yolo3/model.py
@@ -10,6 +10,8 @@ from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.normalization import BatchNormalization
 from keras.models import Model
 from keras.regularizers import l2
+from .load_backend import control_flow_ops
+from .load_backend import set_image_dim_ordering
 
 from yolo3.utils import compose
 
@@ -391,7 +393,7 @@ def yolo_loss(args, anchors, num_classes, ignore_thresh=.5, print_loss=False):
             best_iou = K.max(iou, axis=-1)
             ignore_mask = ignore_mask.write(b, K.cast(best_iou<ignore_thresh, K.dtype(true_box)))
             return b+1, ignore_mask
-        _, ignore_mask = K.control_flow_ops.while_loop(lambda b,*args: b<m, loop_body, [0, ignore_mask])
+        _, ignore_mask = control_flow_ops.while_loop(lambda b,*args: b<m, loop_body, [0, ignore_mask])
         ignore_mask = ignore_mask.stack()
         ignore_mask = K.expand_dims(ignore_mask, -1)
 
