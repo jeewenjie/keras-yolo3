adeaa339cbc5f96ef1f5c65e582f90fbea1723d8
Jee Wen Jie <40534826+jeewenjie@users.noreply.github.com>
2019-10-23 15:24:45 +0800

    Update yolo.py

--- yolo.py
+++ yolo.py
@@ -20,9 +20,9 @@ from keras.utils import multi_gpu_model
 
 class YOLO(object):
     _defaults = {
-        "model_path": 'model_data/yolo.h5',
-        "anchors_path": 'model_data/yolo_anchors.txt',
-        "classes_path": 'model_data/coco_classes.txt',
+        "model_path": 'logs/000/trained_weights_stage_1.h5',
+        "anchors_path": 'anchors.txt',
+        "classes_path": 'classes.txt',
         "score" : 0.3,
         "iou" : 0.45,
         "model_image_size" : (416, 416),
@@ -126,7 +126,7 @@ class YOLO(object):
 
         print('Found {} boxes for {}'.format(len(out_boxes), 'img'))
 
-        font = ImageFont.truetype(font='font/FiraMono-Medium.otf',
+        font = ImageFont.truetype(font='/usr/share/fonts/opentype/font-awesome/FontAwesome.otf',
                     size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
         thickness = (image.size[0] + image.size[1]) // 300
 
